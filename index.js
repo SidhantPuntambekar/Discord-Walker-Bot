171bb2450ba4441375fa5f6745c6a20e7f133a46
Saurabh Totey <SaurabhTotey@gmail.com>
2018-08-11 08:16:35 -0600

    Switched to using NPM request library for sake of cleaner code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const https = require("https");
+const request = require("request");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
@@ -79,42 +79,36 @@ client.on("ready", () => {
     //Schedules the bot to read the weather and ask for walkers in the morning at the query time
     client.setTimeout(() => {
         //Gets the weather from the OpenWeatherMap API
-        https.get(`https://api.openweathermap.org/data/2.5/weather?q=Boulder,us&appid=${process.env.OpenWeatherKey}`, response => {
-            //API response gets accumulated into data
-            let data = '';
-            response.on("data", chunk => data += chunk);
-            //After response has been fully collected, the response gets parsed and the rest of the program continues
-            response.on("end", () => {
-                var weatherInfo = JSON.parse(data);
-                var dateFormat = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
-                //Sets the icon of the bot to an icon of the current weather
-                client.user.setAvatar(`http://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`);
-                //Sends a message on the walking channel with the weather data and asks for who is walking
-                walkingChannel.send(`Good morning everyone! For ${now().toLocaleDateString("en-US", dateFormat)}, the temperature is ${weatherInfo.main.temp}K with a humidity of ${weatherInfo.main.humidity}%. Wind speeds currently are ${weatherInfo.wind.speed}m/s. The weather can be summed up by ${formatArrayToString(weatherInfo.weather.map(weather => weather.description))}! For those who are walking, please react to this message with a ${affirmationEmoji}; other emojis or lack thereof are ignored.`).then(msg => {
-                    //Bot reacts to its own message with the necessary emoji for ease of neighbor use
-                    msg.react(affirmationEmoji);
-                    //At displayTime, bot reads the original message's reactions and displays who is walking; also updates stats for each neighbor
-                    client.setTimeout(() => {
-                        var reactions = msg.reactions.array();
-                        var walkers = [];
-                        for (var i = 0; i < reactions.length; i++) {
-                            var reaction = reactions[i];
-                            if (`${reaction.emoji}` == affirmationEmoji) {
-                                walkers = reaction.users.array().filter(user => "tag" in user && user.tag in neighbors);
-                                break;
-                            }
+        request(`https://api.openweathermap.org/data/2.5/weather?q=Boulder,us&appid=${process.env.OpenWeatherKey}`, (error, response, body) => {
+            var weatherInfo = JSON.parse(body);
+            var dateFormat = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+            //Sets the icon of the bot to an icon of the current weather
+            client.user.setAvatar(`http://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`);
+            //Sends a message on the walking channel with the weather data and asks for who is walking
+            walkingChannel.send(`Good morning everyone! For ${now().toLocaleDateString("en-US", dateFormat)}, the temperature is ${weatherInfo.main.temp}K with a humidity of ${weatherInfo.main.humidity}%. Wind speeds currently are ${weatherInfo.wind.speed}m/s. The weather can be summed up by ${formatArrayToString(weatherInfo.weather.map(weather => weather.description))}! For those who are walking, please react to this message with a ${affirmationEmoji}; other emojis or lack thereof are ignored.`).then(msg => {
+                //Bot reacts to its own message with the necessary emoji for ease of neighbor use
+                msg.react(affirmationEmoji);
+                //At displayTime, bot reads the original message's reactions and displays who is walking; also updates stats for each neighbor
+                client.setTimeout(() => {
+                    var reactions = msg.reactions.array();
+                    var walkers = [];
+                    for (var i = 0; i < reactions.length; i++) {
+                        var reaction = reactions[i];
+                        if (`${reaction.emoji}` == affirmationEmoji) {
+                            walkers = reaction.users.array().filter(user => "tag" in user && user.tag in neighbors);
+                            break;
                         }
-                        if (walkers.length > 1) {
-                            walkingChannel.send(`The cool neighbors today are ${formatArrayToString(walkers.map(user => neighbors[user.tag]))}.`);
-                        } else if (walkers.length == 1) {
-                            walkingChannel.send(`${neighbors[walkers[0].tag]} is a very cool neighbor.`);
-                        } else {
-                            walkingChannel.send("No one is walking today... 🙁");
-                        }
-                        //TODO: collect statistics on who is walking
-                        hasFinished = true;
-                    }, new Date(now().getFullYear(), now().getMonth(), now().getDate(), displayTime.hour, displayTime.minute, 0, 0) - now());
-                });
+                    }
+                    if (walkers.length > 1) {
+                        walkingChannel.send(`The cool neighbors today are ${formatArrayToString(walkers.map(user => neighbors[user.tag]))}.`);
+                    } else if (walkers.length == 1) {
+                        walkingChannel.send(`${neighbors[walkers[0].tag]} is a very cool neighbor.`);
+                    } else {
+                        walkingChannel.send("No one is walking today... 🙁");
+                    }
+                    //TODO: collect statistics on who is walking
+                    hasFinished = true;
+                }, new Date(now().getFullYear(), now().getMonth(), now().getDate(), displayTime.hour, displayTime.minute, 0, 0) - now());
             });
         });
     }, new Date(now().getFullYear(), now().getMonth(), now().getDate(), queryTime.hour, queryTime.minute, 0, 0) - now());
