44f73bc4b1041b146187a2eab7461e39c643e1e8
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2018-10-15 15:45:30 -0600

    Delete indexForecastBot.js

--- indexForecastBot.js
+++
@@ -1,9 +0,0 @@
-//Loads environment variables from .env file
-require("dotenv").load();
-
-//Defines constant data for Discord
-const Discord = require("discord.js");
-const express = require("express");
-const request = require("request");
-const client = new Discord.Client();
-
